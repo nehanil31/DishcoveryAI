8a53c4b8af60573e0d777f6a168658f7e5f66d0e
NEHA ANIL <165749232+nehanil31@users.noreply.github.com>
2025-08-10 20:54:46 +0530

    Update foodie_tour.py

--- server/foodie_tour.py
+++ server/foodie_tour.py
@@ -110,4 +110,4 @@ def generate_tour():
         return jsonify({"success": False, "error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
